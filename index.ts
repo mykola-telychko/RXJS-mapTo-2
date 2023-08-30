745a2b24c343e3f0fdbd43d8dcb29f1149b7ca5e
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 01:39:09 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { fromEvent } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
+
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/mapto
+// Example 2: Mapping clicks to string
+
+//emit every click on document
+const source = fromEvent(document, 'click');
+//map all emissions to one value
+const example = source.pipe(mapTo('GOODBYE WORLD!'));
+const subscribe = example.subscribe(val => console.log(val));
+//output: (click)'GOODBYE WORLD!'...
